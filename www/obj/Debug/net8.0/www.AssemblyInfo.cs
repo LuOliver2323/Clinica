7794edcbd66a868b24cba16989c97d5337679bf9
LuOliver2323 <lucaslg1607@gmail.com>
2024-08-20 20:01:52 -0300

    teste

--- www/obj/Debug/net8.0/www.AssemblyInfo.cs
+++ www/obj/Debug/net8.0/www.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("www")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82bd27319042cdf6a52b3d81ebf7714b3c7c18c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5221cca2c8197886ebc0faa06fab9a04fc0db48")]
 [assembly: System.Reflection.AssemblyProductAttribute("www")]
 [assembly: System.Reflection.AssemblyTitleAttribute("www")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
